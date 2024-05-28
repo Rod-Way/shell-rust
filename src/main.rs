95d78a73f51ae5540c0397a12f15a14fd950232f
Rod-Way <Rodway.work@gmail.com>
2024-05-28 08:27:21 +0400

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -13,6 +9,5 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-    print!("{}: command not found\n", input);
-    io::stdout().flush().unwrap();
+    print!("{}: command not found\n", input.trim());
 }
